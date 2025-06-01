b0e287c05b5babde943519a9e3220956accbde12
Igor <Igor@sql.kk>
2025-06-01 18:18:55 +0200

    Fix: get token from env

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Bot
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Подгрузка переменных из .env (локально, если нужно)
 
 TOKEN = os.getenv("7910658659:AAG-qyMEV5ZiDmlxhhxT5oGimfIKfzEThYY")
 CHAT_ID = os.getenv("336663757")
