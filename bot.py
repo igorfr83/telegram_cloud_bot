1d9f2668b509818c7e7a02e8e8421b54a00e43e9
Igor <Igor@sql.kk>
2025-06-01 18:58:11 +0200

    fix async await

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import os
+import asyncio
 from telegram import Bot
 from dotenv import load_dotenv
 
-load_dotenv()  # Подгрузка переменных из .env (локально, если нужно)
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ Бот работает из облака!")
+async def main():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(chat_id=CHAT_ID, text="✅ Бот работает из облака!")
+
+if name == "__main__":
+    asyncio.run(main())
